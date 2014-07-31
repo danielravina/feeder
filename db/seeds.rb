3f2381e007d0849ef192ef88ad89bb23e29bcb31
DanielRavina <raabhim@gmail.com>
2014-07-31 16:19:23 -0700

    typo

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-Admin::User.create username: "test", password: "123", first_name: "tyler", last_name: "durden"
+User::Admin.create username: "test", password: "123", first_name: "tyler", last_name: "durden"
 p "USER CREATED"
 p "username test"
 p "password: 123"

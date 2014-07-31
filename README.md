9a9ba54d8d2fa5772fb7e1563d23765697c85bbd
DanielRavina <raabhim@gmail.com>
2014-07-31 16:18:10 -0700

    add seed for log in and update readme

---
+++ README.md
@@ -0,0 +1,5 @@
+== README
+
+Please use `foreman start` to start the app and `guard` to run the tests.
+
+To create the admin user please run `rake db:seed`
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,4 @@
-User.create username: "test", password: "123", first_name: "tyler", last_name: "durden"
+Admin::User.create username: "test", password: "123", first_name: "tyler", last_name: "durden"
+p "USER CREATED"
+p "username test"
+p "password: 123"
